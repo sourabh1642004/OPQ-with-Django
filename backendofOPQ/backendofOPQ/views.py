1a74c350f886b8dadec0c6b8768765e097bb098b
sourabh1642004 <139848259+sourabh1642004@users.noreply.github.com>
2024-03-09 01:49:58 +0530

    Update views.py

--- backendofOPQ/backendofOPQ/views.py
+++ backendofOPQ/backendofOPQ/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.shortcuts import render, redirect
+from .models import Student
 
 
 
@@ -27,3 +29,31 @@ def basepage(request):
 
 def Coursedetails(request,courseid):
     return HttpResponse(courseid)
+
+
+
+
+def register_student(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        date_of_birth = request.POST.get('date_of_birth')
+        email = request.POST.get('email')
+        phone_number = request.POST.get('phone_number')
+        address = request.POST.get('address')
+
+        student = Student.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            date_of_birth=date_of_birth,
+            email=email,
+            phone_number=phone_number,
+            address=address
+        )
+        # Redirect to a success page or another URL
+        return redirect('success_page')  # Replace 'success_page' with the appropriate URL name
+    else:
+        return render(request, 'student_registration.html')
+
+
+
