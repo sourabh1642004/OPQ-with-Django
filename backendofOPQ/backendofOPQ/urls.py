4b0e4ddde11d96330a7187515d554c904fdf2b7a
sourabh1642004 <139848259+sourabh1642004@users.noreply.github.com>
2024-03-09 01:52:33 +0530

    Update urls.py

--- backendofOPQ/backendofOPQ/urls.py
+++ backendofOPQ/backendofOPQ/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for backendofOPQ project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from backendofOPQ import views
@@ -24,4 +9,13 @@ urlpatterns = [
     path('base/', views.basepage),
     path('course/<int:courseid>', views.Coursedetails),
     path('', views.indexpage),
+]
+
+
+from django.urls import path
+from .views import register_student
+
+urlpatterns = [
+    path('register/', register_student, name='register_student'),
+    # Add other URL patterns if needed
 ]
