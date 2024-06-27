4f1086605fdc115da9e804459fe24395bde5b71b
sourabh meena <sourabh1642004@gmail.com>
2024-06-27 18:21:55 +0530

    some update

--- backendofOPQdemo/news/admin.py
+++ backendofOPQdemo/news/admin.py
@@ -4,5 +4,6 @@ from news.models import News
 class NewsAdmin(admin.ModelAdmin):
     list_display=('date','title','desc','pdf')
     
+    
 admin.site.register(News,NewsAdmin)
-# Register your models here.
+
